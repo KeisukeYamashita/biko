d24ada318639c1d3ed018349315ce9a1956bcd72
KeisukeYamashita <19yamashita15@gmail.com>
2019-10-04 22:29:22 +0900

    :watermelon: add google provider

---
+++ cli/google.go
@@ -0,0 +1,45 @@
+package cli
+
+import (
+	"github.com/KeisukeYamashita/biko/browser"
+	"github.com/KeisukeYamashita/biko/providers/google"
+	"github.com/urfave/cli"
+)
+
+func newGoogleCmd() cli.Command {
+	return cli.Command{
+		Name:  "google",
+		Usage: "Open GCP resource",
+		Flags: []cli.Flag{
+			cli.StringFlag{
+				Name:  "project",
+				Usage: "Specify the project to open",
+			},
+		},
+		Action: func(c *cli.Context) error {
+			gcp := &google.Provider{}
+			return browser.Open(c, gcp)
+		},
+		Subcommands: []cli.Command{
+			newSearchCmd(),
+		},
+	}
+}
+
+func newSearchCmd() cli.Command {
+	return cli.Command{
+		Name:  "search",
+		Usage: "Search a page",
+		Flags: []cli.Flag{
+			cli.StringFlag{
+				Name:  "query",
+				Usage: "Query a page",
+			},
+		},
+		Action: func(c *cli.Context) error {
+			gcp := &google.Provider{}
+			return browser.Open(c, gcp)
+		},
+	}
+
+}
